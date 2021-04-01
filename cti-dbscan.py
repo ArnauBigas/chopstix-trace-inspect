4ffbbd32b001d680f8f7156e03b9345b9b805910
Arnau Bigas <arnau.bigas@gmail.com>
2021-04-01 17:09:48 +0200

    Fix formatting of cti-dbscan output

--- cti-dbscan.py
+++ cti-dbscan.py
@@ -45,6 +45,6 @@ for trace_file in args.trace_files:
 
     if not args.summary:
         for cluster in clusters:
-            print("Cluster %2d" % cluster)
+            print("Cluster %d" % cluster)
             print("----------")
             print(clusters[cluster])
